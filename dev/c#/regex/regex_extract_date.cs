6d08a2330771f629b425048090841cf6fc5c6156
AlexTian <atflysun@outlook.com>
2017-10-24 22:58:31 +0800

    add

---
+++ dev/c#/regex/regex_extract_date.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var lines = File.ReadAllLines(@"d:/phone.txt");
+           
+        }
+
+    }
+}
